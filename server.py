b47d5c21e13461c17af5013f12448e6eac5f3136
Rajat Shrestha <70558370+sayyanstha@users.noreply.github.com>
2023-12-11 12:08:23 +0545

    Update server.js

--- server.py
+++ server.py
@@ -1,14 +1,38 @@
-from flask import Flask, request, jsonify
-app = Flask(__name__)
+const http = require('http');
+const url = require('url');
 
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    try:
-        expression = request.json['expression']
-        result = eval(expression)
-        return jsonify({'result': result})
-    except Exception as e:
-        return jsonify({'error': str(e)})
+const server = http.createServer((req, res) => {
+  const queryObject = url.parse(req.url, true).query;
+  const num1 = parseFloat(queryObject.num1);
+  const num2 = parseFloat(queryObject.num2);
+  const operator = queryObject.operator;
 
-if __name__ == '__main__':
-    app.run(debug=True)
+  let result;
+
+  switch (operator) {
+    case 'add':
+      result = num1 + num2;
+      break;
+    case 'subtract':
+      result = num1 - num2;
+      break;
+    case 'multiply':
+      result = num1 * num2;
+      break;
+    case 'divide':
+      result = num1 / num2;
+      break;
+    default:
+      result = 'Invalid operator';
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(`Result: ${result}`);
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
